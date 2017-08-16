438ab20c8fa27e435d873fa539b2dfb56b5186a0
Aaron Rohrbacher and Nate McGregor <nate@>
2017-08-16 14:23:10 -0700

    move all consonants at start of word to end of word before ay

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,36 @@
+function isVowel(letter){
+  if(letter === 'a'||letter === 'e'||letter === 'i'||letter === 'o'||letter === 'u'){
+    return true;
+  }else{
+    return false;
+  }
+}
+
 function converter(sentence) {
-  arr = sentence.split(" ");
-  for(i = 0; i < arr.length; ++i) {
+  var arr = sentence.split(" ");
+  var lettersBeforeVowel = "";
+  for(var i = 0; i < arr.length; ++i) {
     var firstLetter = arr[i].charAt(0);
     var word = arr[i];
-
-    if(firstLetter === 'a'||firstLetter === 'e'||firstLetter === 'i'||firstLetter === 'o'||firstLetter === 'u'){
+    if(isVowel(firstLetter)){
       arr[i] = word+"way";
-    }else{
-      word = word.slice(1);
-      arr[i] = word+firstLetter+"ay";
     }
+    if(!(isVowel(firstLetter))){
+      for (var j = 0; j < word.length; ++j) {
 
+        if(isVowel(word[j])){
+          word = word.slice(j);
+          arr[i] = word+lettersBeforeVowel+"ay";
+          lettersBeforeVowel = "";
+        }else {
+          lettersBeforeVowel += word[j];
+        }
+      }
+    }
   }
-
   return arr.join(" ");
 }
+
 
 $(document).ready(function() {
   $("form#pigLatin").submit(function(event) {
